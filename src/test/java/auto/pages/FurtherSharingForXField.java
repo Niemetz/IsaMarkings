5c89c4584475a2ee65cd8a2c9b05a7211c92a6b7
Niemetz <nguyen7744@yahoo.com>
2017-06-16 17:42:38 -0400

    xx

--- src/test/java/auto/pages/FurtherSharingForXField.java
+++ src/test/java/auto/pages/FurtherSharingForXField.java
@@ -4,7 +4,7 @@ public class FurtherSharingForXField extends AmazonBasePageObject {
 	public FurtherSharingForXField()
 	{
 		mapTable.clear();
-		System.out.println("I am in the class => FurtherSharingForXField");
+		//System.out.println("I am in the class => FurtherSharingForXField");
 		mapTable.put("Sign in".toLowerCase(), "//ISA Marking/ForField/&objectID&/Further Sharing/-#signInSubmit");
 		mapTable.put("Email".toLowerCase(), "//ISA Marking/ForField/&objectID&/Further Sharing/-#ap_email");
 		mapTable.put("Password".toLowerCase(), "//ISA Marking/ForField/&objectID&/Further Sharing/-#ap_password");
